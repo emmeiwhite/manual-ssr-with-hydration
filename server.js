55a97e0ae23d2faa1e19da9b5b81d4444aa45bd9
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-06-11 22:33:07 +0530

    sending back htmlTemplate from main root

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
+const { readFileSync } = require('fs')
 const { createServer } = require('http')
 const { parse } = require('url')
 
+const htmlTemplate = readFileSync(`${__dirname}/index.html`)
+
 const server = createServer((req, res) => {
-  res.end('Hello World!')
+  const pathName = parse(req.url, true).pathname
+
+  if (pathName === '/') {
+    res.writeHead(200, { 'Content-type': 'text/html' })
+    res.end(htmlTemplate)
+  } else if (pathName === '/about') {
+    res.end('About Me')
+  } else {
+    res.end('The URL cannot be found')
+  }
 })
 
 server.listen(8000, () => {
